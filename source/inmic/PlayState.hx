fff05c9fe5e73df5f8b4605c36435ded78b43091
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-17 21:55:05 -0500

    pausing + wip editor mode

--- source/inmic/PlayState.hx
+++ source/inmic/PlayState.hx
@@ -13,18 +13,23 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var EDITOR_MODE:Bool = false;
+
+	var PAUSED:Bool = false;
+
 	var song:FlxSound;
 
 	var songName = 'Lead';
-	var songTime(get, null):Float;
+	var songTimeMS(get, null):Float;
 
-	function get_songTime():Float return (song == null) ? 0 : song.time;
+	function get_songTimeMS():Float return (song == null) ? 0 : song.time;
 
-	var songLength(get, null):Float;
+	var songLengthMS(get, null):Float;
 
-	function get_songLength():Float return (song == null) ? 0 : song.length;
+	function get_songLengthMS():Float return (song == null) ? 0 : song.length;
 
 	var timeBar:FlxBar;
+	var timeText:FlxText;
 
 	override function create()
 	{
@@ -33,12 +38,51 @@ class PlayState extends FlxState
 		FlxG.sound.list.add(song = new FlxSound().load('assets/songs/$songName.ogg'));
 		song.play();
 
-		add(timeBar = new FlxBar(0, 0, LEFT_TO_RIGHT, FlxG.width, 16, this, 'songTime', 0, songLength, false));
+		add(timeBar = new FlxBar(0, 0, LEFT_TO_RIGHT, FlxG.width, 16, this, 'songTimeMS', 0, songLengthMS, false));
 		timeBar.createFilledBar(0xFF000000, 0xFF00FF00);
+
+		add(timeText = new FlxText());
+		timeText.screenCenter(X);
+
+		updateEditorMode();
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.ESCAPE) togglePaused();
+		if (FlxG.keys.justPressed.F7) toggleEditorMode();
+
+		timeText.text = '${songTimeMS / 1000} / ${songLengthMS / 1000}';
+	}
+
+	function togglePaused()
+	{
+        if (EDITOR_MODE) return;
+
+		PAUSED = !PAUSED;
+
+		updatePaused();
+	}
+
+	function updatePaused()
+	{
+		if (PAUSED || EDITOR_MODE) if (song.playing) song.pause();
+		if (!PAUSED && !EDITOR_MODE) if (!song.playing) song.play();
+	}
+
+	function toggleEditorMode()
+	{
+		EDITOR_MODE = !EDITOR_MODE;
+
+		updateEditorMode();
+	}
+
+	function updateEditorMode()
+	{
+		updatePaused();
+
+		timeText.alpha = (EDITOR_MODE) ? 0.75 : 0.001;
 	}
 }
